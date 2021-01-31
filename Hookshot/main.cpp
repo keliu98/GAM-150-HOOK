46cd912c12d49e602685cc9a0a14d919144170af
Xxjames98xX <tanweiwen321@gmail.com>
2021-01-31 12:32:46 +0800

    added comment

--- Hookshot/main.cpp
+++ Hookshot/main.cpp
@@ -11,7 +11,7 @@
 */
 // ---------------------------------------------------------------------------
 // main
-
+// added comment
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
