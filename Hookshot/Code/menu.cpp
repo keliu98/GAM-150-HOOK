ca1c4cf3bb4911dcf46574a6d88331a2dc4a97a0
VentusFlame <78005432+VentusFlame@users.noreply.github.com>
2021-03-05 14:35:29 +0800

    Menu Done
    Added texture for menu

--- Hookshot/Code/menu.cpp
+++ Hookshot/Code/menu.cpp
@@ -1,22 +1,36 @@
 #include "pch.h"
 
 AEGfxVertexList* button = 0;
+AEGfxVertexList* text = 0;
+AEGfxTexture * pTex1; 
+
 
 void menu_Load() {
 	AEGfxMeshStart();
 
+
+	// This shape has 2 triangles
 	AEGfxTriAdd(
-		-2.5f, 0.5f, 0x0F52BA, 0.0f, 0.0f,
-		-2.5f, -0.5f, 0x0F52BA, 0.0f, 0.0f,
-		2.5f, 0.5f, 0x0F52BA, 0.0f, 0.0f);
+		-300.0f, -300.0f, 0x00FF00FF, 0.0f, 1.0f,
+		300.0f, -300.0f, 0x00FFFF00, 1.0f, 1.0f,
+		-300.0f, 300.0f, 0x0000FFFF, 0.0f, 0.0f);
 
 	AEGfxTriAdd(
-		2.5f, -0.5f, 0x0F52BA, 0.0f, 0.0f,
-		-2.5f, -0.5f, 0x0F52BA, 0.0f, 0.0f,
-		2.5f, 0.5f, 0x0F52BA, 0.0f, 0.0f);
+		300.0f, -300.0f, 0x00FFFFFF, 1.0f, 1.0f,
+		300.0f,  300.0f, 0x00FFFFFF, 1.0f, 0.0f,
+		-300.0f, 300.0f, 0x00FFFFFF, 0.0f, 0.0f);
+
+	// Saving the mesh (list of triangles) in pMesh2
 
 	button = AEGfxMeshEnd();
+
+	
 	AE_ASSERT_MESG(button, "Failed to create button1!!");
+
+	pTex1 = AEGfxTextureLoad("Title.png");
+	AE_ASSERT_MESG(pTex1, "Failed to create texture1!!");
+
+
 }
 
 void menu_Initialize()
@@ -28,36 +42,33 @@ void menu_Update() {
 	AEInputUpdate();
 	if (AEInputCheckTriggered(AEVK_RETURN))
 		next = GS_LEVEL1;
+	
 }
 
 void menu_Draw() {
-	AEMtx33	trans, scale, rot;
+	
 
-	// Compute the scaling matrix
-	AEMtx33Scale(&scale, button_1->scale, button_1->scale);
-
-	// Compute the translation matrix
-	AEMtx33Trans(&trans, button_1->pos.x, button_1->pos.y);
-
-	AEMtx33Concat(&button_1->transform, &trans, &scale);
-
-	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
-	AEGfxTextureSet(NULL, 0, 0);
-	AEGfxSetTransform(button_1->transform.m);
+	// Drawing object 2 - (first) - No tint
+	AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+	// Set position for object 2
+	AEGfxSetPosition(0.0f, 0.0f);
+	
+	// No tint
+	AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+	// Set texture
+	AEGfxTextureSet(pTex1, 0.0f, 0.0f);
+	// Drawing the mesh (list of triangles)
 	AEGfxMeshDraw(button, AE_GFX_MDM_TRIANGLES);
-
-	if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
-		next = GS_QUIT;
-
 
 
 
 }
 
 void menu_Free() {
-	
+
 }
 
 void menu_Unload() {
 	AEGfxMeshFree(button);
+	AEGfxTextureUnload(pTex1);
 }
