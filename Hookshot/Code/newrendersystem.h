e498bffc9a603f10eb17599b27f803aa680ea717
Xxjames98xX <tanweiwen321@gmail.com>
2021-03-24 23:21:12 +0800

    help me oh god help

--- Hookshot/Code/newrendersystem.h
+++ Hookshot/Code/newrendersystem.h
@@ -5,16 +5,13 @@
 //AEGfxVertexList* load_mesh(float size); //player
 
 void load_bg_render();
-
 void load_dirt_render();
-
 void load_texture_render();
-
 void load_character_render();
-
 void load_enemy_texture();
-
 void load_door_texture();
+void load_hook_render();
+void load_button_texture();
 
 AEGfxTexture* load_texture(const char* image);
 
@@ -25,8 +22,6 @@ void update_render_buttons();
 void update_render_character();
 void update_render_hook();
 void update_render_enemy();
-void load_hook_render();
-void load_button_texture();
 
 void load_render();
 void unload_render();
